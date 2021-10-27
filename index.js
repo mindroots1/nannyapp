5726b04988a3526c012b92f888290b50b2d2c1b3
mrt <rkumar1@mind-roots.com>
2021-10-27 13:07:40 +0530

    changes

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express()
 
 app.get('/',(req, res) => {
-
     res.send("welcome")
+})
+
+app.get('/user',(req, res) => {
+    res.send({
+        "status": true,
+        "message": "Welcome"
+    })
 })
 
 app.listen(8000, () => {
