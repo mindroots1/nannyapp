4621ad169f1b61ccc340aaffc3bb9cf4d60ac966
mrt <rkumar1@mind-roots.com>
2021-10-28 16:58:56 +0530

    create post user api for signup

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ app.get('/user',(req, res) => {
     })
 })
 
+app.post('/user',(req, res) => {
+    
+    let user = User({
+        username: req.body.username,
+        phone: req.body.phone,
+        email: req.body.email,
+        city: req.body.city,
+        state: req.body.state,
+        password: req.body.password
+    })
+    user.save((error, result) => {
+        if (error) {
+            res.json({"status": false, "message": error.message})
+        }
+        else {
+            res.json({"status": true, "message": "signup successfully"})
+        }
+    })
+})
+
 app.listen(port_number, () => {
     console.log(`connected: http://localhost:${port_number}`)
 })
