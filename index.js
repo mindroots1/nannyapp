dea52ae41c367a1eb55dfbc603bdce1053e4d506
mrt <rkumar1@mind-roots.com>
2021-10-28 16:46:07 +0530

    get user list

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+require('./database/dbconnection')
 const port_number = process.env.PORT || 3000
-
 const app = express()
+const User = require('./models/User');
 
 app.get('/',(req, res) => {
     res.status(400).send("this url is not valid")
 })
 
 app.get('/user',(req, res) => {
-    res.send({
-        "status": true,
-        "message": "Welcome"
+    
+    User.find((error, result) => {
+        if (error) {
+            res.json({"status": false, "message": error.message})
+        }
+        else {
+            res.json({"status": true, "data": result})
+        }
     })
 })
 
